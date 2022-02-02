05e9dda5cb870bcaae38c522d78b9677a6287ae8
Patrick Roelofs <contact@patrickroelofs.com>
2022-02-02 12:15:07 +0100

    add bp01

---
+++ bp01-chatapplicatie/bp01-chatapplicatie/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bp01_chatapplicatie
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
