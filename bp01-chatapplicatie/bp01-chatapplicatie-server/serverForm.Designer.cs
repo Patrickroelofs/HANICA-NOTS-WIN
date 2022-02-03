1da6e80b395ad60c7cc32835290e12597dbca3fd
Patrick Roelofs <contact@patrickroelofs.com>
2022-02-03 12:02:29 +0100

    add server and change styling of client

---
+++ bp01-chatapplicatie/bp01-chatapplicatie-server/serverForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace bp01_chatapplicatie_server
+{
+    partial class serverForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "NoTS - Bikini Bottom Server";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
