5ad2d5b8cd9f8691f5c4ee609671771f70cf7176
Patrick Roelofs <contact@patrickroelofs.com>
2022-02-14 15:29:28 +0100

    typo

--- bp01-chatapplicatie/bp01-chatapplicatie/Parsers.cs
+++ bp01-chatapplicatie/bp01-chatapplicatie/Parsers.cs
@@ -17,7 +17,7 @@ namespace bp01_chatapplicatie
         {
             bool tryParseIp = IPAddress.TryParse(ip, out IPAddress ipAddress) || ip == "localhost";
             bool tryValidatePort = port.All(char.IsDigit) && ParseToInt(port) <= 65535 && ParseToInt(port) > 0;
-            bool tryValidateBufferSize = bufferSize.All(char.IsDigit) && ParseToInt(bufferSize) < 65536 && ParseToInt(bufferSize) > 0;
+            bool tryValidateBufferSize = bufferSize.All(char.IsDigit) && ParseToInt(bufferSize) < 65535 && ParseToInt(bufferSize) > 0;
             bool tryValidateName = name.All(char.IsLetterOrDigit) && name.Length > 0 && name.Length < 16;
 
             if (tryParseIp && tryValidatePort && tryValidateBufferSize && tryValidateName) return true;
